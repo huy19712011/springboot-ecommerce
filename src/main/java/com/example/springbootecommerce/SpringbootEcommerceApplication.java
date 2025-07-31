7ff9ebfa5e3429cb52a670b642a9487501752b99
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-07-31 22:16:56 +0700

    Init

---
+++ src/main/java/com/example/springbootecommerce/SpringbootEcommerceApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEcommerceApplication.class, args);
+        System.out.println("running...");
+    }
+
+}
